85a3e12986ef79a1f788cc8e48e6bb426f2dfa35
rodrigo <rodrigocaceres@192.168.1.24>
2025-10-12 23:50:13 -0300

    Update screen changes.

--- app.config.js
+++ app.config.js
@@ -1,10 +1,11 @@
-export default {
-    expo: {
-        name: "recienhecho-ui-app",
-        slug: "recienhecho-ui-app",
-        userInterfaceStyle: "automatic",
-        extra: {
-            API_BASE_URL: process.env.API_BASE_URL || "http://192.168.1.31:8080",
-        },
+export default ({ config }) => ({
+  expo: {
+    ...config,
+    name: "recienhecho-ui-app",
+    slug: "recienhecho-ui-app",
+    userInterfaceStyle: "automatic",
+    extra: {
+      API_BASE_URL: process.env.API_BASE_URL || "http://localhost:8080", // solo dev
     },
-};
+  },
+});
