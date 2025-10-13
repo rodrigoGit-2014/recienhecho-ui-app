1568a83254fa8fd893c8cbee40dac5a841d50334
rodrigo <rodrigocaceres@192.168.1.24>
2025-10-12 23:25:07 -0300

    refactoring

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default {
         name: "recienhecho-ui-app",
         slug: "recienhecho-ui-app",
         extra: {
-            API_BASE_URL: process.env.API_BASE_URL || "http://192.168.1.31:8080",
+            API_BASE_URL: process.env.API_BASE_URL || "https://recienhecho-service-production.up.railway.app",
         },
     },
 };
